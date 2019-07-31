3a274e1db30d646a970a289c843ed03be716abb6
ethan-ou <ethan-ou@users.noreply.github.com>
2019-07-31 17:04:47 +1000

    Works for powerpoint

--- src/main/ipc/compress.js
+++ src/main/ipc/compress.js
@@ -2,18 +2,19 @@ const fs = require('fs');
 const Queue = require('better-queue');
 const path = require('path');
 const JSZip = require("jszip");
-const hbjs = require('handbrake-js');
-const compress_images = require('compress-images');
 
 const imagemin = require('imagemin');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminSvgo = require('imagemin-svgo');
-
+const hbjs = require('handbrake-js');
+const Jimp = require('jimp');
 
 const videoFileTypes = ['.mp4', '.MP4', '.mkv', '.MKV', '.mov', '.MOV', 'm4v', 'M4V'];
 const photoFileTypes = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.svg', '.SVG', '.gif', '.GIF'];
+const photoResizeFileTypes = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG'];
+const webFileTypes = ['.html', '.css', '.js']
 const powerpointFileTypes = ['.pptx'];
 const OUTPUT_path = './output/';
 
@@ -34,45 +35,8 @@ export default class Compress {
         this.compressImages = this.compressImages.bind(this);
         this.compressVideos = this.compressVideos.bind(this);
         this.compressPowerpoint = this.compressPowerpoint.bind(this);
+        this.filterFileTypes = this.filterFileTypes.bind(this);
 
-        this.photoQueue = new Queue(function (files, endTask) {
-            console.log("from photoQueue:", files);
-            try {
-                for (const file of files) {
-                        //const originalBuffer = fs.readFileSync(file);
-                            const image = imagemin([file], { 
-                            destination: OUTPUT_path + path.basename(file),
-                            plugins: imageMinPlugins })
-                            .then(() => {
-                                console.log('Images optimized');
-                                console.log(image)
-                                endTask();
-                            });
-                            
-                }
-              } catch (error) {
-                console.error('Error: ', error);
-              }
-            // this.compressImages(file);
-            // compress_images(file, OUTPUT_path + path.basename(file), {compress_force: false, statistic: true, autoupdate: true}, false,
-            //     {jpg: {engine: 'mozjpeg', command: ['-quality', '80']}},
-            //     {png: {engine: 'pngquant', command: ['--quality=50-70']}},
-            //     {svg: {engine: 'svgo', command: '--multipass'}},
-            //     {gif: {engine: 'gifsicle', command: ['--colors', '256', '--use-col=web']}},
-            //     function(error, completed, statistic){
-            //         console.log('-------------');
-            //         console.log(error);
-            //         console.log(completed);
-            //         console.log(statistic);
-            //         console.log('-------------');
-            //         if (completed == true) {
-            //             endTask();
-            //         }
-            //     });
-            
-            console.log("compress!")
-            
-        }, { batchSize: 10, concurrent: 10 })
         this.videoQueue = new Queue(function (file, endTask) {
             console.log("from videoQueue:", file)
             hbjs.spawn({ 
@@ -97,54 +61,24 @@ export default class Compress {
 
     async addToQueue(event, files) {
         try {
-            console.log(this.photoQueue);
             console.log("Number of files:", files.length);
+            console.log(files)
             for (const file of files) {
                 if (photoFileTypes.includes(path.extname(file))) {
-                    // compress_images(file, OUTPUT_path + file.name, {compress_force: false, statistic: true, autoupdate: true}, false,
-                    //     {jpg: {engine: 'mozjpeg', command: ['-quality', '80']}},
-                    //     {png: {engine: 'pngquant', command: ['--quality=50-70']}},
-                    //     {svg: {engine: 'svgo', command: '--multipass'}},
-                    //     {gif: {engine: 'gifsicle', command: ['--colors', '256', '--use-col=web']}},
-                    //     function(error, completed, statistic){
-                    //         console.log('-------------');
-                    //         console.log(error);
-                    //         console.log(completed);
-                    //         console.log(statistic);
-                    //         console.log('-------------');
-                    //     });
-                    // await this.photoQueue.push(file)
                     await this.compressImages(file);   
                 }
                 if (videoFileTypes.includes(path.extname(file))) {
                     await this.videoQueue.push(file);   
                 }
-
                 if (powerpointFileTypes.includes(path.extname(file))) {
                     await this.compressPowerpoint(file);
                 }
             }   
         }
         catch(error) {
-            console.error('Error: ', error);
+            console.error('Error:', error);
         }
     }
-
-    // async compressImages(file) {
-    //     console.log("From compress images", file)
-    //     compress_images(file, OUTPUT_path + file.name, {compress_force: false, statistic: true, autoupdate: true}, false,
-    //         {jpg: {engine: 'mozjpeg', command: ['-quality', '80']}},
-    //         {png: {engine: 'pngquant', command: ['--quality=50-70']}},
-    //         {svg: {engine: 'svgo', command: '--multipass'}},
-    //         {gif: {engine: 'gifsicle', command: ['--colors', '256', '--use-col=web']}},
-    //         function(error, completed, statistic){
-    //             console.log('-------------');
-    //             console.log(error);
-    //             console.log(completed);
-    //             console.log(statistic);
-    //             console.log('-------------');
-    //         });
-    // }
 
     compressVideos(file, endTask) {
         hbjs.spawn({ 
@@ -166,60 +100,77 @@ export default class Compress {
 
     async compressImages(file) {
         try {
-            const originalBuffer = await fs.readFile(file, async function read(err, data) {
-                if (err) {
-                    throw err;
-                }
+            await fs.readFile(file, async function read(err, data) {
+                if (err) throw err;
                 console.log(data);
                 let image = await imagemin.buffer(data, { plugins: imageMinPlugins })
                 console.log(image)
                 await fs.writeFile(OUTPUT_path + path.basename(file), image, function(err) {
-                    if(err) {
-                        return console.log(err);
-                    }
+                    if (err) throw err;
                     console.log("Image Processed")
                 });
             });
-            
-
-        } catch (error) {
+        } catch(error) {
           console.error('Error: ', error);
         }
       }
 
     async compressPowerpoint(file) {
-        const mediaFiles = [];
-        let newFile = OUTPUT_path + path.basename(file)
-        fs.createReadStream(file).pipe(fs.createWriteStream(newFile));
-        console.log(newFile)
-        fs.readFile(newFile, function(err, data) {
+        let mediaFiles = [];
+        fs.readFile(file, function(err, data) {
             if (err) throw err;
-            JSZip.loadAsync(data).then(async function (zip) {
+            JSZip.loadAsync(data).then(async (zip) => {
+                let allFiles = Object.keys(zip.files);
+                let filteredFiles = await this.filterFileTypes(allFiles);
+                let photoFiles = allFiles.filter((file) => {
+                    if (photoFileTypes.includes(path.extname(file))) { return file };
+                });
+                // let videoFiles = allFiles.filter((file) => {
+                //     if (videoFileTypes.includes(path.extname(file))) { return file };
+                // })
+         
+                console.log("Object.keys:", allFiles);
+                console.log("Photo Files:", photoFiles);
+                console.log("Filtered Files:", filteredFiles);
+
                 zip.folder("ppt/media").forEach((relativePath, file) => {
-                    mediaFiles.push(relativePath);
+                    if (photoFileTypes.includes(path.extname(relativePath))) {
+                        mediaFiles.push(relativePath);
+                    }
                 });
                 console.log("Number of files:", mediaFiles.length);
                 
                 for (const file of mediaFiles) {
                     //console.log(zip.file("ppt/media/" + file).async("nodebuffer"))
-                    
+                    console.log(file);
+                    //console.log(mediaFiles);
                     await zip.file("ppt/media/" + file).async("nodebuffer", function updateCallback(metadata) {
                         console.log("progression: " + metadata.percent.toFixed(2) + " %")
                     }).then(async (content) => {
                         let image = await imagemin.buffer(content, { plugins: imageMinPlugins })
                         console.log(image);
                         console.log("ppt/media/" + file)
-
-                        //CURRENTLY NOT WRITING TO PPTX FILE!!!
+                        console.log("1")
+                        //Writes to memory representation of zip file
                         let zipFile = zip.file("ppt/media/" + file, image, {binary: true})
-                        console.log(zipFile)
+                        //console.log(zipFile)
                         //fs.writeFile("output/"+ file, image, function(err){/*...*/});
+                        console.log("2");
+                        
                         
                     })
+                    console.log(file);
                     
                     //let image = await imagemin.buffer(buffer, { plugins: imageMinPlugins })
                     
                 }
+
+                //Saves memory representation to hard drive when all processing is done
+                zip.generateAsync({type:"nodebuffer"})
+                    .then(function(content) {
+                            // Force down of the Zip file
+                        fs.writeFile(OUTPUT_path + path.basename(file), content, function(err){/*...*/});
+                    });
                 // zip.folder("ppt/media").generateAsync({type: "nodebuffer"}).then(async function (content) {
                 //     let image = await imagemin.buffer(content, { plugins: imageMinPlugins })
                 //     console.log(image)
@@ -241,6 +192,25 @@ export default class Compress {
               console.log(files);
             });
         });
+    }
+
+    filterFileTypes(files) {
+        
+            let photoFiles = files.filter((file) => {
+                if (photoFileTypes.includes(path.extname(file))) { return file };
+            });
+            let videoFiles = files.filter((file) => {
+                if (videoFileTypes.includes(path.extname(file))) { return file };
+            });
+            let webFiles = files.filter((file) => {
+                if (webFileTypes.includes(path.extname(file))) { return file };
+            })
+
+            return {
+                photo: photoFiles,
+                video: videoFiles,
+                web: webFiles
+            };
     }
 
 }
