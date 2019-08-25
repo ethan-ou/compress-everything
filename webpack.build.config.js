01f9de5ab031a04bd555c2c94605e46a3e62b47d
Ethan Ou <ethantim@gmail.com>
2019-08-25 22:31:15 +1000

    Added tailwind css

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -24,7 +24,8 @@ module.exports = {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader'
+          use: [{ loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader']
         }),
         include: defaultInclude
       },
